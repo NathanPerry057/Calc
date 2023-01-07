4c1e523f0ed9e613565b5b7095e090e08aef7c7a
NathanPerry057 <G00394355@atu.ie>
2023-01-07 16:08:49 +0000

    Feature mul added to project in feature mul

--- src/main/java/ie/atu/week4/Calc.java
+++ src/main/java/ie/atu/week4/Calc.java
@@ -1,7 +1,21 @@
 package ie.atu.week4;
 
+import java.util.Scanner;
+
 public class Calc {
     public static void main(String[] args) {
+        mul();
+    }
 
+    public static void mul() {
+        System.out.println("Please enter your first number");
+        Scanner inputs = new Scanner(System.in);
+        int firstNumber = inputs.nextInt();
+
+        System.out.println("Please enter your second number");
+        int secondNumber = inputs.nextInt();
+
+        int total = firstNumber * secondNumber;
+        System.out.println("The total is " + total);
     }
 }
