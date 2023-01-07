ed0c0879dfc15d9658acefaef57bdf7dc21c5288
NathanPerry057 <G00394355@atu.ie>
2023-01-07 16:01:51 +0000

    Feature delete added to project

--- src/main/java/ie/atu/week4/Calc.java
+++ src/main/java/ie/atu/week4/Calc.java
@@ -1,7 +1,21 @@
 package ie.atu.week4;
 
+import java.util.Scanner;
+
 public class Calc {
     public static void main(String[] args) {
+        delete();
+    }
 
+    public static void delete() {
+        System.out.println("Please enter a number");
+        Scanner inputs = new Scanner(System.in);
+        int firstNumber = inputs.nextInt();
+
+        System.out.println("Subtract by ");
+        int secondNumber = inputs.nextInt();
+
+        int total = firstNumber - secondNumber;
+        System.out.println("The total is " + total);
     }
 }
